ee4abbb09f33d8b9feecfa7d99d4f43f69eaaa75
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-03-04 19:45:45 -0600

    Update db.py

--- src/services/db.py
+++ src/services/db.py
@@ -1,11 +1,4 @@
 import mysql.connector as rds
-from dotenv import load_dotenv 
-import os
-
-
-# Load the .env file
-load_dotenv()
-database=os.getenv("DB_NAME")
         
 # Get the value of a variable
 def make_connection():
