7ade216f123bd5993f5140f030ac36168d5520c6
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-03-04 20:38:06 -0600

    Update user.py

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -4,7 +4,8 @@ from database.r import get_user_data
 
 def is_valid_user(data):
     # Validar que los campos no estén vacíos
-    if len(data.get('username')) == 0 or len(data.get('password')) == 0 or len(data.get('mail')) == 0 or len(data.get('fullname')) == 0 :
+    if len(data.get('username')) == 0 or len(data.get('password')) == 0 or len(data.get('mail')) == 0 or len(data.get('fullname')) == 0:
+        print("Faltan campos en el request.")
         return JSONResponse(
             status_code=400,
             content={
@@ -13,6 +14,7 @@ def is_valid_user(data):
         )
 
     if get_user_data(data.get('username')) != None:
+        print("El usuario ya existe.")
         return JSONResponse(
             status_code=400,
             content={
