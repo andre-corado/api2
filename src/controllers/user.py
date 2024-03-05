ab3e6343c9bfc4738ee356fc18e5cfb19a46f72f
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-03-04 20:34:21 -0600

    Update user.py

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -4,7 +4,7 @@ from database.r import get_user_data
 
 def is_valid_user(data):
     # Validar que los campos no estén vacíos
-    if len(data.get('username')) == 0 or len(data.get('password')) == 0 or len(data.get('mail')) == 0 or len(data.get('fullname')) == 0 or len(data.get('imageB64')) == 0:
+    if len(data.get('username')) == 0 or len(data.get('password')) == 0 or len(data.get('mail')) == 0 or len(data.get('fullname')) == 0 :
         return JSONResponse(
             status_code=400,
             content={
