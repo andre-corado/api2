6e49d44cc31b3f8f28b982b41ec399d54721c80a
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-03-05 18:02:56 -0600

    Update s3.py

--- src/services/s3.py
+++ src/services/s3.py
@@ -5,11 +5,12 @@ import re
 # Configurar el nombre del bucket
 
 # Crear un cliente S3
-s3 = boto3.client('s3', access_key_id)
 bucket_name = 'faunadexp1'
 aws_access_key_id = 'AKIAVRUVRXKCQWV4TE5C'
 aws_secret_access_key = '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'
 location = 'east-1'
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
 
 
 def upload_file_to_s3(image_b64, filename):
