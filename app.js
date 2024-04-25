3417d32057d542e3a90f3ad97ff73058d79abaf9
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-04-24 20:27:24 -0600

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import indexRoutes from "./routes/index.routes.js";
 import unoRoutes from "./routes/3.routes.js";
@@ -27,6 +28,8 @@ var corsOptions = {
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use("/", indexRoutes);
--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 import app from "./app.js";
 import { PORT } from "./config/config.js";
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 app.listen(PORT);
 console.log(`Server on port http://localhost:${PORT}`);
